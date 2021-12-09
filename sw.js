8d5955fa39a89060897771cc68e4f31343c8c7b2
Lee, Joshua <82845425+joshuleedel@users.noreply.github.com>
2021-12-09 17:50:32 -0500

    Initial commit

---
+++ sw.js
@@ -0,0 +1,37 @@
+addEventListener('fetch', fetchEvent => {
+  console.log('fetching', fetchEvent.request);
+  fetchEvent.respondWith((async () => {
+    if (fetchEvent.request.mode === 'navigate' &&
+      fetchEvent.request.method === 'GET' &&
+      registration.waiting &&
+      (await clients.matchAll()).length < 2
+    ) {
+      registration.waiting.postMessage('skipWaiting');
+      return new Response("", {headers: {"Refresh": "0"}});
+    }
+
+    return await caches.match(fetchEvent.request) ||
+      fetch(fetchEvent.request);
+  })());
+});
+
+const LATEST_CACHE_ID = 'v1';
+
+addEventListener('install', installEvent => {
+  installEvent.waitUntil(
+    caches.open(LATEST_CACHE_ID).then(cache => cache.addAll['/']))
+});
+
+addEventListener('activate', activateEvent => {
+  activateEvent.waitUntil(
+    caches.keys().then(keyList => Promise.all(keyList.map(key => {
+      if (key !== LATEST_CACHE_ID) {
+        return caches.delete(key);
+      }
+    })))
+  );
+});
+
+addEventListener('message', messageEvent => {
+  if (messageEvent.data === 'skipWaiting') return skipWaiting();
+});
